8ad03170b6caab8e34a5d08ab692a60f5e7dfd5e
knight008848 <vincent.yi1984@gmail.com>
2017-09-21 16:15:51 +0800

    Change a field as not null in user

--- apps/user/models.py
+++ apps/user/models.py
@@ -9,7 +9,7 @@ from datetime import datetime
 class UserProfile(AbstractUser):
     birth_day = models.DateField(verbose_name='birthday', null=True, blank=True)
     gend = models.CharField(choices=(('m', 'male'), ('f', 'female')), default='m', max_length=4, verbose_name='gender')
-    address = models.CharField(max_length=100, null=True, verbose_name='address')
+    address = models.CharField(max_length=100, null=True, blank=True, verbose_name='address')
     image = models.ImageField(upload_to='image/%Y/%m', default=u'image/default.png', null=True, blank=True)
 
     class Meta:
