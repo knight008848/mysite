94d6a5751af190b52d97352d1f8e92b56bf57132
knight008848 <vincent.yi1984@gmail.com>
2017-10-18 14:35:25 +0800

    Update admin style of cfi app

--- apps/cfiman/adminx.py
+++ apps/cfiman/adminx.py
@@ -1,22 +1,23 @@
-from django.contrib import admin
+# -*- coding:utf-8 -*-
+
+import xadmin
+
 from .models import *
 
-# Register your models here.
 
-
-class CfiAdmin(admin.ModelAdmin):
+class CfiAdmin(object):
     list_display = ['account', 'add_time', 'last_modify', 'size']
     search_fields = ['account']
     ordering = ('-last_modify',)
     date_hierarchy = 'add_time'
 
 
-class PatchAdmin(admin.ModelAdmin):
+class PatchAdmin(object):
     list_display = ['number', 'add_time', 'is_active']
     search_fields = ['number']
     ordering = ('-add_time',)
     date_hierarchy = 'add_time'
 
 
-admin.site.register(cfinote, CfiAdmin)
-admin.site.register(patch, PatchAdmin)
+xadmin.site.register(cfinote, CfiAdmin)
+xadmin.site.register(patch, PatchAdmin)
